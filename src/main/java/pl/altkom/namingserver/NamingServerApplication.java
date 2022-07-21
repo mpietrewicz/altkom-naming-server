c1d7bb4ce3d78f7879941b7b68c3026f13bb2ada
mpietrewicz <mpietrewicz@gmail.com>
2022-07-21 15:07:47 +0200

    Konfiguracja eureka

--- src/main/java/pl/altkom/namingserver/NamingServerApplication.java
+++ src/main/java/pl/altkom/namingserver/NamingServerApplication.java
@@ -2,8 +2,11 @@ package pl.altkom.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NamingServerApplication {
 
 	public static void main(String[] args) {
